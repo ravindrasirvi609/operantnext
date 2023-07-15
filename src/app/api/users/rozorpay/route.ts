5156b12e50d7c39569ea5e3e72a9441dabdad088
ravindra <sirviravindra609@gmail.com>
2023-07-15 23:14:25 +0530

    rozerpay added

---
+++ src/app/api/users/rozorpay/route.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Razorpay from "razorpay";
+import shortid from "shortid";
+
+
+export async function POST(req:NextApiRequest, res:NextApiResponse) {
+  console.log(req);
+  
+  if (req.method === 'POST') {
+    // Initialize razorpay object
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+    console.log("razorpay",razorpay);
+    
+
+    // Create an order -> generate the OrderID -> Send it to the Front-end
+    // Also, check the amount and currency on the backend (Security measure)
+    const payment_capture = 1;
+    const amount = 499;
+    const currency = "INR";
+    const options = {
+      amount: (amount * 100).toString(),
+      currency,
+      receipt: shortid.generate(),
+      payment_capture,
+    };
+
+    try {
+      const response = await razorpay.orders.create(options);
+      console.log("---------------------------------------------------",response);
+      const data = {
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      }
+      console.log("data",data);
+      
+      res.status(200).json({
+        id: response.id,
+        currency: response.currency,
+        amount: response.amount,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  } else {
+    // Handle any other HTTP method
+  }
+}
---
+++ src/app/rozorpay/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+
+import Head from "next/head";
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function Home() {
+  const makePayment = async () => {
+    console.log("here...");
+    const res = await initializeRazorpay();
+
+    if (!res) {
+      alert("Razorpay SDK Failed to load");
+      return;
+    }
+
+    // Make API call to the serverless API
+    const data = await fetch("/api/users/rozorpay", { method: "POST" }).then((t) =>
+      t.json()
+    );
+    console.log(data);
+    var options = {
+      key: process.env.RAZORPAY_KEY, // Enter the Key ID generated from the Dashboard
+      name: "Manu Arora Pvt Ltd",
+      currency: data.currency,
+      amount: data.amount,
+      order_id: data.id,
+      description: "Thankyou for your test donation",
+      image: "https://manuarora.in/logo.png",
+      handler: function (response) {
+        // Validate payment at server - using webhooks is a better idea.
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature);
+      },
+      prefill: {
+        name: "Manu Arora",
+        email: "manuarorawork@gmail.com",
+        contact: "9999999999",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  };
+  const initializeRazorpay = () => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = "https://checkout.razorpay.com/v1/checkout.js";
+      // document.body.appendChild(script);
+
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+  return (
+    <div>
+      <Head>
+        <title>Integrate Payments 🔥</title>
+        <meta
+          name="description"
+          content="Integrate payments in your React and Next.js application with TailwindCSS and Razorpay"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="relative z-10 flex flex-col md:flex-row mt-10 items-center max-w-6xl justify-evenly mx-auto">
+        <div className="md:w-1/3 mb-20 md:mb-0 mx-10">
+          <div className="bg-gradient-to-r from-[#3e4044] to-[#1D2328] p-[1px] rounded-md mb-4">
+            <button
+              onClick={makePayment}
+              
+              
+            >
+              payment 
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
