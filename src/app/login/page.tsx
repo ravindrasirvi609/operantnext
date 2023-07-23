1ec214ccfa1d187467b89c5c1928626dd716eece
ravindra <sirviravindra609@gmail.com>
2023-07-23 18:59:09 +0530

    Login page Style change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Image from "next/image";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -85,43 +86,135 @@ export default function LoginPage() {
   }, [user]);
 
   return (
-    <div className="font-Inter h-screen overflow-auto bg-gradient-to-tr from-[#31c14e] to-[#1a3e85]">
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>{loading ? "Processing" : "Login"}</h1>
-        <hr />
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Image
+          className="mx-auto h-15.5rem w-auto"
+          src="/opf.png"
+          alt="OPF Logo"
+          height={1000} // The height of the image in pixels
+          width={1000} // The width of the image in pixels
+        />
 
-        <label htmlFor="email">email</label>
-        <input
-          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-          id="email"
-          type="text"
-          value={user.email}
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="email"
-        />
-        <label htmlFor="password">password</label>
-        <input
-          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-          id="password"
-          type="password"
-          value={user.password}
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="password"
-        />
-        <button
-          onClick={onLogin}
-          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-        >
-          Login here
-        </button>
-        <button
-          onClick={ForgotPassword}
-          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-        >
-          Forgot Password
-        </button>
-        <Link href="/signup">Visit Signup page</Link>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          {loading ? "Processing" : "Sign in to your account"}
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className=" p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <button
+                  onClick={ForgotPassword}
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </button>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                autoComplete="current-password"
+                required
+                className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              onClick={onLogin}
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <a
+            href="/signup"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            New Registration
+          </a>
+        </p>
       </div>
     </div>
+
+    // <div className="font-Inter h-screen overflow-auto bg-gradient-to-tr from-[#31c14e] to-[#1a3e85]">
+    //   <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    //     <h1>{loading ? "Processing" : "Login"}</h1>
+    //     <hr />
+
+    //     <label htmlFor="email">email</label>
+    //     <input
+    //       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+    //       id="email"
+    //       type="text"
+    //       value={user.email}
+    //       onChange={(e) => setUser({ ...user, email: e.target.value })}
+    //       placeholder="email"
+    //     />
+    //     <label htmlFor="password">password</label>
+    //     <input
+    //       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+    //       id="password"
+    //       type="password"
+    //       value={user.password}
+    //       onChange={(e) => setUser({ ...user, password: e.target.value })}
+    //       placeholder="password"
+    //     />
+    //     <button
+    //       onClick={onLogin}
+    //       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+    //     >
+    //       Login here
+    //     </button>
+    //     <button
+    //       onClick={ForgotPassword}
+    //       className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+    //     >
+    //       Forgot Password
+    //     </button>
+    //     <Link href="/signup">Visit Signup page</Link>
+    //   </div>
+    // </div>
   );
 }
