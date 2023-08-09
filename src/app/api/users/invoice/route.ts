8609a98bef6146c96a47dc42bbab3661ad55c3c4
ravindra <sirviravindra609@gmail.com>
2023-08-09 23:06:47 +0530

    warning Fixes

--- src/app/api/users/invoice/route.ts
+++ src/app/api/users/invoice/route.ts
@@ -3,45 +3,53 @@ import puppeteer from "puppeteer";
 
 // Define a function to generate the invoice HTML content
 function generateInvoiceHtml(orderId: string /* ...other data... */): string {
-  // Your logic to generate the invoice HTML content
-  // Make sure to replace this with your actual HTML template
   const invoiceHtml = `
-    <html>
-      <head>
-        <!-- Your HTML head content -->
-      </head>
-      <body>
-        <!-- Your HTML body content -->
-        <h1>Invoice for Order ${orderId}</h1>
-        <!-- ...other invoice details... -->
-      </body>
-    </html>
-  `;
+      <html>
+        <head>
+          <style>
+            /* Your CSS styles */
+            body {
+              font-family: Arial, sans-serif;
+            }
+            .invoice-header {
+              text-align: center;
+              margin-bottom: 20px;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="invoice-header">
+            <h1>Invoice for Order ${orderId}</h1>
+          </div>
+          <div class="invoice-details">
+            <p>Order ID: ${orderId}</p>
+            <!-- ...other invoice details... -->
+          </div>
+        </body>
+      </html>
+    `;
   return invoiceHtml;
 }
 
 export async function POST(req: NextRequest) {
-    console.log("POST request", req);
-    
+  console.log("POST request", req);
+
   if (req.method === "POST") {
     try {
       // Extract order ID from the URL query parameters
       const url = new URL(req.url);
-      const orderId = url.searchParams.get("id") || "defaultOrderId"; // Replace with your logic to get the order ID
+      const orderId = url.searchParams.get("id") || "defaultOrderId";
       console.log("Extracting order ID from URL", url.searchParams);
-
       // Generate the invoice PDF using Puppeteer
       const invoiceHtml = generateInvoiceHtml(orderId /* ...other data... */);
-
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        headless: "new",
+      });
       const page = await browser.newPage();
+      console.log("Generated Invoice HTML:", invoiceHtml);
       await page.setContent(invoiceHtml);
-
       const pdfBuffer = await page.pdf();
-
       await browser.close();
-
-      // Attach the PDF buffer to the response
       return new NextResponse(pdfBuffer, {
         headers: {
           "Content-Disposition": `attachment; filename="invoice_${orderId}.pdf"`,
