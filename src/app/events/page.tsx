2ef54cb0667a4bef0ac2edeb3738de1b9f600c19
Ravindra <sirviravindra609@gmail.com>
2023-11-07 23:53:12 +0530

    event Listing Css Change

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -60,77 +60,64 @@ export default function EventList() {
       <h1 className="bg-purple-900 rounded-lg p-2 text-3xl font-semibold mb-4 text-white">
         Event List
       </h1>
-      <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500	 rounded-lg p-6 shadow-md w-full sm:w-3/4 md:w-2/3 lg:w-1/2">
-        {eventData ? (
-          <div>
-            {eventData.map((event, index) => (
-              <div key={event._id}>
-                <p className="mb-2">
-                  <h1 className="font-extrabold	 text-4xl	 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500	text-black	capitalize">
-                    {event.title}
-                  </h1>
-                </p>
-                <div className="flex justify-between">
-                  <p className="mb-3 p-2 bg-red-200 rounded-lg text-center	decoration-zinc-100	decoration-solid	underline-offset-auto	">
-                    <a
-                      href={event.registrationUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-black"
-                    >
-                      Register here
-                    </a>
-                  </p>
-
-                  <p className="mb-2">
-                    <span className="font-semibold flex justify-end">
-                      {formatDate(event.date)}
-                    </span>
-                    <div>
-                      {" "}
-                      {event.location.address}, {event.location.city},{" "}
-                      {event.location.state},{event.location.country}
-                    </div>
-                  </p>
-                </div>
-                {event.isPaid ? (
-                  <p className="mb-2">
-                    <span className="font-semibold">Price:</span> {event.price}
-                  </p>
-                ) : (
-                  <p className="mb-2">
-                    <span className="font-semibold">Price:</span> Free
-                  </p>
-                )}
-                <p className="mb-2">
-                  <span className="font-semibold">Categories</span>{" "}
-                  {event.categories}
-                </p>
-
-                <p className="mb-2">
-                  <span className="font-semibold">Categories</span>{" "}
-                  {event.categories}
+  
+      {eventData ? (
+        <div>
+          {eventData.map((event) => (
+            <div
+              key={event._id}
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg p-6 shadow-md w-90 align-center  m-5"
+            >
+              <h1 className="font-extrabold text-4xl text-center text-black capitalize">
+                {event.title}
+              </h1>
+              <div className="flex justify-between">
+                <p className="mb-3 p-2 bg-red-200 rounded-lg text-center decoration-zinc-100 decoration-solid underline-offset-auto">
+                  <a
+                    href={event.registrationUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-black"
+                  >
+                    Register here
+                  </a>
                 </p>
                 <p className="mb-2">
-                  <span className="font-semibold">Capacity</span>{" "}
-                  {event.capacity} Members
-                </p>
-
-                <p className="mb-2">
-                  <span className="font-semibold flex italic">
-                    {event.description}
+                  <span className="font-semibold flex justify-end">
+                    {formatDate(event.date)}
                   </span>
+                  <div>
+                    {event.location.address}, {event.location.city},{" "}
+                    {event.location.state}, {event.location.country}
+                  </div>
                 </p>
               </div>
-            ))}
-          </div>
-        ) : (
-          <p>Loading event data...</p>
-        )}
-      </div>
-      {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">
-          Logout
-        </button> */}
+              {event.isPaid ? (
+                <p className="mb-2">
+                  <span className="font-semibold">Price:</span> {event.price}
+                </p>
+              ) : (
+                <p className="mb-2">
+                  <span className="font-semibold">Price:</span> Free
+                </p>
+              )}
+              <p className="mb-2">
+                <span className="font-semibold">Categories:</span>{" "}
+                {event.categories.join(', ')}
+              </p>
+              <p className="mb-2">
+                <span className="font-semibold">Capacity:</span> {event.capacity} Members
+              </p>
+              <p className="mb-2">
+                <span className="font-semibold italic">Description:</span> {event.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading event data...</p>
+      )}
     </div>
   );
+  
 }
