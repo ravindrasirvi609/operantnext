38d9c42484ca8fd4ca9ec14082e1172da236e196
Ravindra <sirviravindra609@gmail.com>
2023-11-15 15:50:55 +0530

    formating fixes

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,21 +1,16 @@
 import { NextResponse } from "next/server";
 
-
 // This is the logout route, it will clear the token cookie and local storage
 export async function GET() {
-    try {
-        const response = NextResponse.json(
-            {
-                message: "Logout successful",
-                success: true,
-            }
-        )
-        response.cookies.set("token", "", 
-        { httpOnly: true, expires: new Date(0) 
-        });
-        localStorage.clear();
-        return response;
-    } catch (error: any) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+  try {
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
+    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    localStorage.clear();
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
