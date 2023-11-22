8e4c1790ca1586879ee1616ea72e8cbb95736c6b
Ravindra <sirviravindra609@gmail.com>
2023-11-22 23:55:38 +0530

    log out API changes

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
 
-// This is the logout route, it will clear the token cookie and local storage
+// This is the logout route, it will clear the token cookie
 export async function GET() {
   try {
     const response = NextResponse.json({
       message: "Logout successful",
       success: true,
     });
+
+    // Clear the token cookie
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-    localStorage.clear();
+
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
