81058c1ba1d7fbc194b18b1926cd94af6d893e76
Ravindra <sirviravindra609@gmail.com>
2023-11-30 23:08:10 +0530

    event List Changes

--- src/app/api/events/eventLists/route.ts
+++ src/app/api/events/eventLists/route.ts
@@ -1,6 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import eventModel from "@/models/eventModel";
+import Organizer from "@/models/organizerModel";
 import { NextRequest, NextResponse } from "next/server";
 
 // Initialize the database connection
@@ -18,7 +19,6 @@ export async function GET(req: NextRequest) {
     }
 
     // Step 3: Retrieve events from the database
-    // You may want to add additional authorization and query conditions here.
     const events = await eventModel.find(/* add query conditions if needed */);
 
     if (!events || events.length === 0) {
@@ -27,13 +27,44 @@ export async function GET(req: NextRequest) {
     }
 
     // Step 4: Check if the user has already joined each event
-    const eventsWithJoinStatus = events.map((event) => {
-      const isUserJoined = event.attendees.includes(userId);
-      return { ...event.toObject(), isJoin: isUserJoined };
-    });
+    const eventsWithJoinStatus = await Promise.all(
+      events.map(async (event) => {
+        const isUserJoined = event.attendees.includes(userId);
+
+        try {
+          // Fetch organizer details
+          const organizer = await Organizer.findById(event.organizer);
+
+          if (!organizer) {
+            console.log(`Organizer not found for event ${event._id}`);
+            return null; // or handle the case where organizer is not found
+          }
+
+          const organizerName = organizer.userName; // Use the correct field from your schema
+          const organizerEmail = organizer.email;
+
+          console.log("Organizer name", organizerName);
+
+          return {
+            ...event.toObject(),
+            isJoin: isUserJoined,
+            organizerDetails: {
+              name: organizerName,
+              email: organizerEmail,
+            },
+          };
+        } catch (error) {
+          console.error(`Error fetching organizer for event ${event._id}:`, error);
+          return null; // or handle the error case
+        }
+      })
+    );
+
+    // Filter out null values (events without organizers)
+    const validEvents = eventsWithJoinStatus.filter((event) => event !== null);
 
     // Step 5: Handle successful response
-    return new NextResponse(JSON.stringify(eventsWithJoinStatus), {
+    return new NextResponse(JSON.stringify(validEvents), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -26,6 +26,10 @@ interface Event {
   capacity: number;
   attendees: string[];
   __v: number;
+  organizerDetails: {
+    name: string;
+    email: string;
+  };
 }
 
 export default function EventList() {
@@ -35,7 +39,6 @@ export default function EventList() {
   const getEventList = async () => {
     try {
       const res = await axios.get("/api/events/eventLists");
-      console.log("res", res);
       setEventData(res.data); // Set 'eventData' with response data
     } catch (error) {
       toast.error("Failed to get event details");
@@ -56,10 +59,10 @@ export default function EventList() {
   }
 
   return (
-    <div className="bg-lime-100 absolute inset-x-0 top-0 z-50">
+    <div className="bg-gradient-to-b from-purple-500 to-blue-600 absolute inset-x-0 top-0 z-50">
       <div className="flex flex-col items-center justify-center min-h-screen mt-2">
-        <h1 className=" p-2 text-3xl font-extrabold mb-4 text-sky-800">
-          Upcoming Events
+        <h1 className="p-4 text-5xl font-extrabold mb-6 text-white">
+          Discover Upcoming Events
         </h1>
 
         {eventData ? (
@@ -67,51 +70,77 @@ export default function EventList() {
             {eventData.map((event) => (
               <div
                 key={event._id}
-                className="text-black bg-sky-200 border-green-600 shadow-lg shadow-green-950	 border-2	 p-6 w-90 align-center  m-5"
+                className="text-white bg-gray-800 border-green-600 shadow-lg border-2 p-6 w-auto align-center m-5 rounded-md"
               >
-                <h1 className="font-extrabold text-4xl text-center text-black capitalize">
+                <h1 className="font-extrabold text-4xl text-center capitalize mb-8">
                   {event.title}
                 </h1>
-                <div className="flex justify-between">
-                  <p className="m-3 p-2 bg-violet-700 rounded-lg	 font-extrabold hover:bg-violet-300 text-white hover:text-black">
-                    <Link href="/rozorpay">Join here</Link>
+                <div className="flex justify-between mb-6">
+                  <p className="m-3 p-2 bg-indigo-700 rounded-lg font-extrabold hover:bg-indigo-300 text-white hover:text-black">
+                    <Link href={`/events/${event._id}`}>Details & RSVP</Link>
                   </p>
                   <p className="mb-2">
-                    <span className="font-semibold flex justify-end">
+                    <span className="font-semibold flex justify-end mb-2">
                       {formatDate(event.date)}
                     </span>
-                    <div>
+                    <div className="text-gray-400">
                       {event.location.address}, {event.location.city},{" "}
                       {event.location.state}, {event.location.country}
                     </div>
                   </p>
                 </div>
                 {event.isPaid ? (
-                  <p className="mb-2">
-                    <span className="font-semibold">Price:</span> {event.price}
+                  <p className="mb-2 m-3">
+                    <span className="text-yellow-400 text-2xl	font-bold bg-gray-950 rounded-lg p-2">
+                      {event.price}
+                    </span>
                   </p>
                 ) : (
-                  <p className="mb-2">
-                    <span className="font-semibold">Price:</span> Free
+                  <p className="mb-2 m-3">
+                    <span className="text-green-400 font-bold text-2xl bg-gray-950 rounded-lg p-2">
+                      Free
+                    </span>
                   </p>
                 )}
-                <p className="mb-2">
+                <p className="mb-2 mt-6">
                   <span className="font-semibold">Categories:</span>{" "}
-                  {event.categories.join(", ")}
+                  {event.categories.map((category) => (
+                    <span
+                      key={category}
+                      className="mr-2 p-1 bg-indigo-500 rounded-md"
+                    >
+                      {category}
+                    </span>
+                  ))}
                 </p>
                 <p className="mb-2">
                   <span className="font-semibold">Capacity:</span>{" "}
-                  {event.capacity} Members
+                  <span className="text-yellow-400">{event.capacity}</span>{" "}
+                  Members
                 </p>
-                <p className="mb-2">
-                  <span className="font-semibold italic">Description:</span>{" "}
+                <p className="m-2">
+                  <span className="font-semibold italic text-justify">
+                    Description:
+                  </span>{" "}
                   {event.description}
                 </p>
+
+                {/* Additional Information */}
+                <div className="mt-4">
+                  <p className="text-xl font-semibold text-yellow-500 mb-2">
+                    Hosted by {event.organizerDetails.name}
+                  </p>
+                  <p className="text-gray-300">
+                    Join us for an incredible experience! Connect with industry
+                    experts, learn new skills, and be part of a vibrant
+                    community. Don t miss out!
+                  </p>
+                </div>
               </div>
             ))}
           </div>
         ) : (
-          <p>Loading event data...</p>
+          <p className="text-white">Loading event data...</p>
         )}
       </div>
     </div>
