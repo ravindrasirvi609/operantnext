83e2cac85c5208d5c48a43e9499365f837f48640
Ravindra Choudhary <84620599+ravindrasirvi609@users.noreply.github.com>
2023-12-04 23:32:34 +0530

    Update route.ts and log removed

--- src/app/api/events/addEvent/route.ts
+++ src/app/api/events/addEvent/route.ts
@@ -9,7 +9,6 @@ connect();
 export async function POST(req: NextRequest) {
   try {
     // Step 1: Get the user's ID from the token
-    console.log("Request ----------------------------------------------------------------");
     
     const userId = await getDataFromToken(req);
 
@@ -17,16 +16,11 @@ export async function POST(req: NextRequest) {
     if (!userId) {
       return new NextResponse("Authentication required", { status: 401 });
     }
-    console.log("Authentication", userId);
 
     // Step 3: Retrieve event data from the request body
     const eventData = await req.json();
-    console.log("Event data", eventData);
-
     // Step 4: Create a new event document in the database
     const event = await eventModel.create({ ...eventData, organizer: userId });
-    console.log("Event", event);
-
     // Step 5: Return the created event as a JSON response
     return new NextResponse(JSON.stringify(event), {
       status: 201, // 201 Created
