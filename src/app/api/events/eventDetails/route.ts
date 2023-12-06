dbeba5cd0240f75989a71bea20093c11790a7404
Ravindra <sirviravindra609@gmail.com>
2023-12-07 02:18:06 +0530

    Add UserForm model and fetch attendees for event

--- src/app/api/events/eventDetails/route.ts
+++ src/app/api/events/eventDetails/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import eventModel from "@/models/eventModel";
 import Plans from "@/models/pricePlansModel";
+import UserForm from "@/models/userForm";
 import { NextRequest, NextResponse } from "next/server";
 
 // Initialize the database connection
@@ -32,8 +33,28 @@ export async function POST(req: NextRequest) {
         return new NextResponse("Plan not found", { status: 404 });
       }
 
+      const ObjectId = require("mongoose").Types.ObjectId;
+
+      const attendeeIds = event.attendees.map((id: string) => new ObjectId(id));
+
+      // Use find() to get an array of users
+      const users = await UserForm.find({ _id: { $in: attendeeIds } });
+
+      if (!users || users.length === 0) {
+        console.log(`Users not found for event ${event._id}`);
+        return new NextResponse("Users not found", { status: 404 });
+      }
+
       const price = plan.price; // Use the correct field from your schema
       const currency = plan.currency; // Use the correct field from your schema
+
+      const attendees = users.map((user) => ({
+        userName: user.userName,
+        personalEmail: user.personalEmail,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        _id: user._id,
+      }));
 
       const responsePayload = {
         ...event.toObject(),
@@ -41,12 +62,10 @@ export async function POST(req: NextRequest) {
           price: price,
           currency: currency,
         },
+        attendees: attendees,
       };
 
-      return new NextResponse(JSON.stringify(responsePayload), {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      });
+      return new NextResponse((JSON.stringify(responsePayload))) ;
     } catch (error) {
       console.error(`Error fetching plan for event ${event._id}:`, error);
       return new NextResponse("Error fetching plan details", { status: 500 });
