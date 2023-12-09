3a3de8010b1b95246cb5ff218784e6f61cb6a848
Ravindra <sirviravindra609@gmail.com>
2023-12-09 16:36:51 +0530

    Add user authentication and role check in addEvent route

--- src/app/api/events/addEvent/route.ts
+++ src/app/api/events/addEvent/route.ts
@@ -1,6 +1,7 @@
 import { connect } from "@/dbConfig/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import eventModel from "@/models/eventModel";
+import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
 // Initialize the database connection
@@ -8,19 +9,25 @@ connect();
 
 export async function POST(req: NextRequest) {
   try {
-    // Step 1: Get the user's ID from the token
-    
-    const userId = await getDataFromToken(req);
+    // Step 1: Ensure the user is authenticated
+    const userId = await getDataFromToken(req);    
+    const user = await User.findById(userId);
 
-    // Step 2: Ensure the user is authenticated
-    if (!userId) {
+    if (!user) {
       return new NextResponse("Authentication required", { status: 401 });
+    }
+
+    // Step 2: Check if the user has the "organization" role
+    if (user.role !== "organization") {
+      return new NextResponse("Unauthorized. User must have the 'organization' role.", { status: 401 });
     }
 
     // Step 3: Retrieve event data from the request body
     const eventData = await req.json();
+
     // Step 4: Create a new event document in the database
     const event = await eventModel.create({ ...eventData, organizer: userId });
+
     // Step 5: Return the created event as a JSON response
     return new NextResponse(JSON.stringify(event), {
       status: 201, // 201 Created
@@ -33,7 +40,14 @@ export async function POST(req: NextRequest) {
 
     if (error.name === "ValidationError") {
       // Handle validation errors (e.g., required fields missing)
-      return new NextResponse(JSON.stringify({ error: error.message }), { status: 400 });
+      return new NextResponse(JSON.stringify({ error: error.message }), {
+        status: 400,
+      });
+    } else if (error.name === "MongoError" && error.code === 11000) {
+      // Handle duplicate key error (e.g., unique constraint violation)
+      return new NextResponse(JSON.stringify({ error: "Duplicate key error" }), {
+        status: 409, // 409 Conflict
+      });
     } else {
       // Handle other errors
       return new NextResponse("Error creating the event", { status: 500 });
