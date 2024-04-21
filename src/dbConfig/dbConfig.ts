2a1afcde7c9673ea19998004ae50541b09693f6b
Ravindra <sirviravindra609@gmail.com>
2024-04-21 08:00:38 +0530

    Refactor MongoDB connection logic in dbConfig.ts

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,24 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+let isConnected = false;
 
 export async function connect() {
-    try {
-       await mongoose.connect(process.env.MONGO_URI!);
-        const connection = mongoose.connection;
+  try {
+    if (!isConnected) {
+      await mongoose.connect(process.env.MONGO_URI!);
+      const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully');
-        })
+      connection.on("connected", () => {
+        isConnected = true;
+        console.log("MongoDB connected successfully");
+      });
 
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-            process.exit();
-        })
-
-    } catch (error) {
-        console.log('Something goes wrong!');
-        console.log(error);
-        
+      connection.on("error", (err) => {
+        console.log(
+          "MongoDB connection error. Please make sure MongoDB is running. " +
+            err
+        );
+        process.exit();
+      });
+    } else {
+      console.log("MongoDB is already connected");
     }
-
-
+  } catch (error) {
+    console.log("Something goes wrong!");
+    console.log(error);
+  }
 }
