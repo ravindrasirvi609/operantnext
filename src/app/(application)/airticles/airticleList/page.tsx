2b3bf95fb7fa736d55abc1d54596cf76f64d4ad2
Ravindra <sirviravindra609@gmail.com>
2024-04-29 22:21:15 +0530

    Add ArticleList component

---
+++ src/app/(application)/airticles/airticleList/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ArticleList: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Article List</h1>
+      {/* Add your article list content here */}
+    </div>
+  );
+};
+
+export default ArticleList;
