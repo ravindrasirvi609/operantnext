1d9ce57a1f52f561c52e24cf36e03c0f9a7f48da
Ravindra Choudhary <84620599+ravindrasirvi609@users.noreply.github.com>
2024-04-30 21:28:00 +0530

    Update page.tsx

--- src/app/(application)/airticles/airticleList/page.tsx
+++ src/app/(application)/airticles/airticleList/page.tsx
@@ -4,7 +4,7 @@ const ArticleList: React.FC = () => {
   return (
     <div className="container mx-auto">
       <h1 className="text-2xl font-bold mb-4">Article List</h1>
-      {/* Add your article list content here */}
+     
     </div>
   );
 };
